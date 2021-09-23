822ecf012343a03fdfd503317bcaec27790dd134
Wenuka19 <89344987+Wenuka19@users.noreply.github.com>
2021-09-23 11:56:32 +0530

    CSEGROUPPRJ: Added a function to get the factorial

--- src/test/java/it/uom/cse/MathOperationTest.java
+++ src/test/java/it/uom/cse/MathOperationTest.java
@@ -16,4 +16,13 @@ class MathOperationTest {
     void subtract() {
         assertEquals(MathOperation.subtract(2, 1), 1);
     }
+
+    public static int factorial(int n)
+    {
+        if (n == 0)
+            return 1;
+
+        return n * factorial(n - 1);
+    }
+
 }
