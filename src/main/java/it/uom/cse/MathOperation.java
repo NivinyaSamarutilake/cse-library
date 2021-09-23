bf16e59266d209513bb622fad7e8748f93be9bf0
PasiduSupunsara <k.a.p.supunsara@gmail.com>
2021-09-23 12:25:58 +0530

    sumOfPrimes method

--- src/main/java/it/uom/cse/MathOperation.java
+++ src/main/java/it/uom/cse/MathOperation.java
@@ -14,5 +14,22 @@ public class MathOperation {
     {
         return operand1 - operand2;
     }
+    public static int sumOfPrimes(int n){
+        int i, number, count, sum = 0;
 
+        for(number = 1; number <= n; number++) {
+            count = 0;
+            for (i = 2; i <= number / 2; i++) {
+                if (number % i == 0) {
+                    count++;
+                    break;
+                }
+            }
+            if (count == 0 && number != 1) {
+                sum = sum + number;
+            }
+        }
+
+        return sum;
+    }
 }
